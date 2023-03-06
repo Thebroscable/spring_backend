07eb5eef5ad022eced610013f709933ebdd142dd
Thebroscable <57013863+Thebroscable@users.noreply.github.com>
2023-03-06 15:06:19 +0100

    Update V2.0__create_store.product_category.sql

--- src/main/resources/db/migration/V2.0__create_store.product_category.sql
+++ src/main/resources/db/migration/V2.0__create_store.product_category.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS store.product_category CASCADE;
 
 CREATE TABLE store.product_category (
-    id      bigserial NOT NULL,
-    name    VARCHAR(100),
-    "desc"  TEXT
+    id              bigserial NOT NULL,
+    name            VARCHAR(100),
+    description     TEXT
 );
 
 ALTER TABLE store.product_category
