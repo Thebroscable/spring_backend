294f11231a1b51ddaab13e590db9b061bcc84613
Thebroscable <57013863+Thebroscable@users.noreply.github.com>
2023-03-08 00:01:18 +0100

    update nameing

--- src/main/resources/db/migration/V7.0__create_users.user_role.sql
+++ src/main/resources/db/migration/V7.0__create_users.user_role.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users.user_role CASCADE;
 CREATE TABLE users.user_role (
     id             bigserial NOT NULL,
     name           VARCHAR(100) NOT NULL,
-    descriptions   TEXT
+    description    TEXT
 );
 
 ALTER TABLE users.user_role ADD CONSTRAINT user_role_pk PRIMARY KEY ( id );
