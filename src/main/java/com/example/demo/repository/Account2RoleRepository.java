b9edbedbbf50ac2d041971156c8477b762b6d391
Thebroscable <57013863+Thebroscable@users.noreply.github.com>
2023-03-08 15:05:56 +0100

    create user repositories

---
+++ src/main/java/com/example/demo/repository/Account2RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Account2Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Account2RoleRepository extends JpaRepository<Long, Account2Role> {
+}
