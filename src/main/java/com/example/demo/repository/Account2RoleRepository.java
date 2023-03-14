1d67ae9906ed0a64938f9696d458ffed901afbff
Thebroscable <57013863+Thebroscable@users.noreply.github.com>
2023-03-14 15:45:18 +0100

    delete user2role files
    they will be replaced

--- src/main/java/com/example/demo/repository/Account2RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Account2Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Account2RoleRepository extends JpaRepository<Account2Role, Long> {
-}
